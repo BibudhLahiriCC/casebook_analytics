3f87e9b38943b1f7e4fd88a3fbd9780e8c6b43b9
Bibudh Lahiri <bibudh@casecommons.org>
2012-04-06 13:28:02 -0400

    Visualization for Caseworker Contacts  metric added

---
+++ eligibility/length_of_status.R
@@ -0,0 +1,78 @@
+customHistogram <- function(histogram, mainTitle, xLabel,
+                            yLabel, fiveNumberSummary)
+{
+  #If there are n bars in the histogram, then 
+  #histogram$breaks is an array of (n+1) points, including
+  #the start-point of first bucket and last point of last bucket.
+  #histogram$counts is an array of n numbers.
+  nBars <- length(histogram$counts);
+  totalFreq <- sum(histogram$counts);
+  heights <- histogram$counts/totalFreq;
+  widths <- c();
+  barLabels <- c();
+  xAxisRightEnd <- max(histogram$breaks);
+  width <- ceiling(xAxisRightEnd/nBars);
+  for (i in 1:nBars)
+  {
+    widths[i] <- width;
+    #barLabels[i] <- as.character(histogram$breaks[i+1]);
+    leftPoint <- 0;
+    if (i > 1)
+    {
+      leftPoint <- histogram$breaks[i] + 1; 
+    }
+    barLabels[i] <- paste(as.character(leftPoint),
+                          "-",
+                          as.character(histogram$breaks[i+1]));
+  }
+  subTitle <- paste("Min = ", fiveNumberSummary[1],
+                    ", Q1 = ", fiveNumberSummary[2],
+                    ", Median = ", fiveNumberSummary[3],
+                    ", Q3 = ", fiveNumberSummary[4],
+                    ", Max = ", fiveNumberSummary[5], sep = "");
+  barplot(height = heights, width = widths, xlim = c(0, xAxisRightEnd),
+          beside = TRUE, horiz = FALSE, main = mainTitle, xlab = xLabel,
+          ylab = yLabel, space = 0, axisnames = TRUE, cex.names = 0.8, 
+          names.arg = barLabels, 
+          sub = subTitle
+          );
+}
+
+length_of_status <- function()
+{
+  library(RPostgreSQL);
+  con <- dbConnect(PostgreSQL(), user="bibudh", 
+          host="staging-db-slave.c45223.blueboxgrid.com", 
+          port="5432",dbname="casebook2_staging");
+  png("length_of_status.png");
+  par(mfrow=c(2, 1));
+  statement <- paste("select b.person_id, current_date - max(date(contacts.occurred_at))", 
+   " from physical_location_records, physical_location_placements, contact_people a,", 
+   " contact_people b, relationships, relationship_types, contacts",
+   " where physical_location_records.person_id = b.person_id",
+   " and physical_location_records.physical_location_id = physical_location_placements.id",
+   " and physical_location_records.physical_location_type = 'PhysicalLocation::Placement'",
+   " and date(physical_location_placements.start_date) <= current_date",
+   " and (date(physical_location_placements.end_date) is NULL or date(physical_location_placements.end_date) > current_date)",
+   " and a.contact_id = b.contact_id and a.person_id <> b.person_id",
+   " and a.person_id = relationships.strong_side_person_id and b.person_id = relationships.weak_side_person_id",
+   " and relationships.type_id = relationship_types.id and relationship_types.strong_name = 'parent' and relationship_types.weak_name = 'child'",
+   " and a.contact_id = contacts.id",
+   " group by b.person_id",
+              sep = "");
+  #str(statement);
+  res <- dbSendQuery(con, statement);
+  data <- fetch(res, n = -1);
+  edges <- c(0, 100, 200, 300, 400, 500, 1000, 2000, 3000, 4000, 5000, 6000);
+  histogram <- hist(data[,2], breaks = edges, plot = FALSE);
+  #str(histogram);
+  customHistogram(histogram = histogram, 
+         mainTitle = paste("#days since last visit with parent for", 
+                           nrow(data), "children", sep = " "),
+         xLabel = "#days since last visit", yLabel = "Fraction of children",
+         fivenum(data[,2]));
+  boxplot(x = data[,2], outline = FALSE);
+  dbDisconnect(con);
+  dev.off();
+}
+
---
+++ embedded_metrics/caseworker_contacts/out_of_home_population.R
@@ -0,0 +1,148 @@
+customHistogram <- function(histogram, mainTitle, xLabel,
+                            yLabel, fiveNumberSummary, queryPoint)
+{
+  #If there are n bars in the histogram, then 
+  #histogram$breaks is an array of (n+1) points, including
+  #the start-point of first bucket and last point of last bucket.
+  #histogram$counts is an array of n numbers.
+  nBars <- length(histogram$counts);
+  totalFreq <- sum(histogram$counts);
+  heights <- histogram$counts/totalFreq;
+  widths <- c();
+  barLabels <- c();
+  xAxisRightEnd <- max(histogram$breaks);
+  width <- ceiling(xAxisRightEnd/nBars);
+  for (i in 1:nBars)
+  {
+    widths[i] <- width;
+    #barLabels[i] <- as.character(histogram$breaks[i+1]);
+    leftPoint <- 0;
+    if (i > 1)
+    {
+      leftPoint <- histogram$breaks[i] + 1; 
+    }
+    barLabels[i] <- paste(as.character(leftPoint),
+                          "-",
+                          as.character(histogram$breaks[i+1]));
+  }
+  subTitle <- paste("As of ", queryPoint, ", min = ", fiveNumberSummary[1],
+                    ", Q1 = ", fiveNumberSummary[2],
+                    ", Median = ", fiveNumberSummary[3],
+                    ", Q3 = ", fiveNumberSummary[4],
+                    ", Max = ", fiveNumberSummary[5], sep = "");
+  barplot(height = heights, width = widths, xlim = c(0, xAxisRightEnd),
+          beside = TRUE, horiz = FALSE, main = mainTitle, xlab = xLabel,
+          ylab = yLabel, space = 0, axisnames = TRUE, cex.names = 0.8, 
+          names.arg = barLabels, 
+          sub = subTitle
+          );
+}
+
+resolve_race <- function(american_indian, asian, black, pacific_islander, white,
+                         multi_racial)
+{
+  race <- "";
+  #str(paste("multi_racial = ", multi_racial, " is of type ", class(multi_racial), 
+  #          sep = ""));
+  #str(paste("white = ", white, " is of type ", class(white), 
+  #          sep = ""));
+  if (isTRUE(multi_racial))
+  {
+   race <- "multi_racial";
+  }
+  else if (isTRUE(american_indian))
+  {  
+   race <- "american_indian";
+  }
+  else if (isTRUE(asian))
+  {  
+   race <- "asian";
+  }
+  else if (isTRUE(black))
+  {  
+   race <- "black";
+  }
+  else if (isTRUE(white))
+  {  
+   race <- "white";
+  }
+  else 
+  {  
+   race <- "unknown";
+  }
+  return(race);
+}
+
+out_of_home_population <- function(queryPoint)
+{
+  library(RPostgreSQL);
+  con <- dbConnect(PostgreSQL(), user="bibudh", host="mirror.in.mycasebook.org", 
+                   port="5432", dbname="casebook2_mirror");
+  png("out_of_home_population.png");
+  par(mfrow=c(2, 1));
+  statement <- paste("select p.id person_id, r.type,",
+                     "to_char(p.date_of_birth, 'YYYY-MM-DD') date_of_birth,",
+                     "p.gender, p.american_indian, p.asian, p.black,",
+                     "p.pacific_islander, p.white, p.multi_racial, p.hispanic_or_latino_origin, cn.name,",
+                      "to_char(c.occurred_at, 'YYYY-MM-DD') last_visit_date,", 
+                      "current_date - date(c.occurred_at) days_since_last_visit,",
+                      "cn.name county_for_case, r.provider_type",
+                    "from removal_locations r, people p, contacts c, contact_people cp,",
+                    "case_plan_focus_children cpfc, case_plans cl, cases cs, counties cn",
+                    "where r.type not in ('RemovalLocation::TrialHomeVisit', 'PhysicalLocation::Runaway')",
+                    "and r.person_id = p.id",
+                    "and r.start_date is not null",
+                    "and (r.end_date is null or date(r.end_date) > current_date)",
+                    "and (cp.person_id = p.id)",
+                    "and (cp.contact_id = c.id)",
+                    "and c.mode like 'Face to Face%'",
+                    "and cpfc.person_id = p.id",
+                    "and cpfc.case_plan_id = cl.id",
+                    "and cl.case_id = cs.id",
+                    "and cs.county_id = cn.id",
+                    "order by p.id, last_visit_date desc",
+                    sep = " ");
+  #cat(statement);
+  res <- dbSendQuery(con, statement);
+  data <- fetch(res, n = -1);
+  #Aliases in SQL become column names in R data frame
+
+  #For each child, keep the last visit date only, and the number of days since 
+  #that date.
+  child_id <- 0;
+  rows_fetched <- nrow(data);
+  output <- data.frame();
+  today <- Sys.Date();
+
+  for (i in 1:rows_fetched)
+  {
+    if (data[i, "person_id"] != child_id)
+    {
+      #New child begins
+      child_id <- data[i, "person_id"];
+      row <- data[i, ];
+      output <- rbind(output, row);
+    }
+  }
+  output["age_rounded_in_years"] <- 
+    round(as.numeric(difftime(today, as.POSIXlt(output[, "date_of_birth"], 
+                                     format="%Y-%m-%d"), units = c("days")))/365);
+                         
+  #output["race"] <- resolve_race(output[, "american_indian"],
+  #                               output[, "asian"],
+  #                               output[, "black"],
+  #                               output[, "pacific_islander"],
+  #                               output[, "white"],
+  #                               output[, "multi_racial"]);
+  output$race <- apply(output, 1, 
+        function(row) resolve_race(as.logical(row["american_indian"]),
+                                   as.logical(row["asian"]),
+                                   as.logical(row["black"]),
+                                   as.logical(row["pacific_islander"]),
+                                   as.logical(row["white"]),
+                                   as.logical(row["multi_racial"])));
+  dbDisconnect(con);
+  dev.off();
+  output;
+}
+
